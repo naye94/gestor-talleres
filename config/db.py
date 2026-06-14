725ce841d89e2cda9c74583834994a768aa07411
naye94 <nayelhy94@gmail.com>
2026-06-14 13:19:38 -0400

    Update db.py

--- config/db.py
+++ config/db.py
@@ -3,7 +3,7 @@ from supabase import create_client, Client
 
 SUPABASE_URL = "https://esaghoyruqyqqjvvfzeu.supabase.co"
 # Pega aquí tu clave anon public real (la que empieza con eyJ...)
-SUPABASE_KEY = "TU_CLAVE_LARGA_ANON_PUBLIC_DE_SUPABASE_AQUÍ" 
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzYWdob3lydXF5cXFqdnZmemV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzODU1MTcsImV4cCI6MjA5Njk2MTUxN30.AQTmAy3EaBPdHfev8298mPakbFa06fF5d_lIqKQ2MCQ" 
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
