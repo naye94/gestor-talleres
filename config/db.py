6b9016d79caf215c9fc7bfd31c51e4d35b093b20
naye94 <nayelhy94@gmail.com>
2026-06-14 13:02:38 -0400

    Refactor database connection to use Supabase client
    Updated the get_connection function to return the Supabase client instead of a MySQL connection.

--- config/db.py
+++ config/db.py
@@ -1,23 +1,11 @@
 import os
 from supabase import create_client, Client
 
-# Pon tus credenciales reales aquí
-SUPABASE_URL = "https://esaghoyruqyqqjvvfzeu.supabase.co" 
-SUPABASE_KEY = "sb_publishable__y5-GG7L89wspXsi8dj_TA_ShyxFQ29" # Tu llave completa
+SUPABASE_URL = "https://esaghoyruqyqqjvvfzeu.supabase.co"
+# Pega aquí tu clave anon public real (la que empieza con eyJ...)
+SUPABASE_KEY = "TU_CLAVE_LARGA_ANON_PUBLIC_DE_SUPABASE_AQUÍ" 
 
-# Creamos la instancia global del cliente
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-import mysql.connector
-from mysql.connector import errorcode
-import re
-
 
 def get_connection():
-   return mysql.connector.connect(
-        host="localhost",
-        port=3306, 
-        user="root",
-        password="1234",
-        database="prueba",
-
-    )
+    return supabase
