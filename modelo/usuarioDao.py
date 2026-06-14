3a08ef2e223a441553c26f1615d8cb1f34e5f936
naye94 <nayelhy94@gmail.com>
2026-06-14 13:33:28 -0400

    Update usuarioDao.py

--- modelo/usuarioDao.py
+++ modelo/usuarioDao.py
@@ -1,4 +1,5 @@
-from config.db import supabase  # 📌 SIEMPRE en la línea 1 y sin espacios al inicio
+# -*- coding: utf-8 -*-
+from config.db import supabase  # SIEMPRE en la linea 1 y sin espacios al inicio
 
 class UsuarioDao:
     @staticmethod
@@ -8,7 +9,7 @@ class UsuarioDao:
                 "email": email,
                 "password": password
             })
-            # Retornamos toda la respuesta de autenticación de forma segura
+            # Retornamos toda la respuesta de autenticacion de forma segura
             return auth_response
         except Exception as e:
             print(f"Error detectado en UsuarioDao: {e}")
